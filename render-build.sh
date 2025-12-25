01240e0f95afc414c8740f5b881a1c0977e5f586
janetsbowers-prog <janet.s.bowers@gmail.com>
2025-12-25 12:45:39 -0800

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
-# Install system dependencies
-apt-get update
+set -o errexit
+
+# Update package lists
+apt-get update -y
+
+# Install Tesseract OCR
 apt-get install -y tesseract-ocr
 
 # Install Python dependencies
 pip install --upgrade pip
 pip install -r requirements.txt
+```
+
+3. **Make sure it has executable permissions** by also creating a file called `.gitattributes` with:
+```
+render-build.sh text eol=lf
